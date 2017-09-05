bd1d7d2589544467a6dd94294d9d7ec69c5d377b
Arthur Vaverko <Arthur.Vaverko@kaltura.com>
2017-09-05 14:20:48 +0300

    Added test project and a nuget package

---
+++ ConsoleMenuTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMenuTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var items = Enumerable.Range(1,20).Select(i => new ConsoleMenuItem($"Item{i}", itemCallback));
+			var menu = new ConsoleMenu("This is a test menu", items);
+			menu.RunConsoleMenu();
+		}
+
+		private static void itemCallback(string itemClicked)
+		{
+			Console.Clear();
+			Console.WriteLine(itemClicked);
+		}
+	}
+}
